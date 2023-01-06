2c498004c2b8d32722e96ba2e68f0de07b14cd52
senoralikz <alexjm825@gmail.com>
2023-01-05 20:55:56 -0500

    added key to projects map

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -20,9 +20,9 @@ const Projects = ({ projects }: Props) => {
       </h3>
 
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#008080]/80">
-        {projects.map((project, index) => (
+        {projects?.map((project) => (
           <div
-            key={index}
+            key={project._id}
             className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen"
           >
             <motion.img

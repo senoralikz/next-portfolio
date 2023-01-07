39c85a549287f8bd9db9a4b3fd39f9d9def00c3d
senoralikz <alexjm825@gmail.com>
2023-01-07 15:24:59 -0500

    added res check in fetchPageInfo

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -5,6 +5,11 @@ export const fetchPageInfo = async () => {
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
   );
 
+  // check res for error
+  if (!res.ok) {
+    console.log(res.statusText);
+  }
+
   const data = await res.json();
   const pageInfo: PageInfo = data.pageInfo;
 

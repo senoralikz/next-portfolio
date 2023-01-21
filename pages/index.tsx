6f9b515bb3ef1393f4825ca908cdd624c2ccbdf5
senoralikz <alexjm825@gmail.com>
2023-01-21 13:35:11 -0500

    uninstalled tailwind scrollbar package and updated favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,6 +29,24 @@ const Home = ({ pageInfo, skills, projects, socials }: Props) => {
     <div className="bg-[#242424] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0">
       <Head>
         <title>Alexander Morales - Portfolio</title>
+        {/* <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        /> */}
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <Header socials={socials} />

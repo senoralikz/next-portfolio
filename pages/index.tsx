de3f2bd07e10ccca720531f16132f0457ecaf38d
senoralikz <alexjm825@gmail.com>
2023-01-21 14:15:02 -0500

    edited links in head for favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,22 +30,22 @@ const Home = ({ pageInfo, skills, projects, socials }: Props) => {
       <Head>
         <title>Alexander Morales - Portfolio</title>
         {/* <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
           rel="icon"
           type="image/png"
           sizes="32x32"
           href="/favicon-32x32.png"
-        />
-        <link
+        /> */}
+        {/* <link
           rel="icon"
           type="image/png"
           sizes="16x16"
           href="/favicon-16x16.png"
         /> */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
